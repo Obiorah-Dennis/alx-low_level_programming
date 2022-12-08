c25aa870f9d5db93610e1dbbd507504cdf6715c8
Obiorah-Dennis <obiorahdennis@yahoo.com>
2022-12-08 03:59:42 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - main block
- *return: #
+ *Return: 0
  */
 int main(void)
-{ 	puts(*\'Programming is like building a multilingual puzzle');
-	return (#);
+{ 	puts(*\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
