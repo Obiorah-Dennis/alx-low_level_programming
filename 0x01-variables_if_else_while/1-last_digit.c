d03c57742be1a685c5a3d673a2ce709fbbd25ee6
Obiorah-Dennis <obiorahdennis@yahoo.com>
2022-12-12 06:57:51 -0800

    The last Number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,23 +6,25 @@
  * description: Get a random number and print
  * Return: 0 (sucess)
  */
+
 int main(void)
 {
-srand(time(NULL));
-int n = rand();
-int last_digit = n % 10;
-printf("Last digit of %d is ", n);
-if (last_digit > 5)
-{
-printf("and is greater than 5\n");
-}
-else if (last_digit == 0)
-{
-printf("and is 0\n");
-}
-else
-{
-printf("and is less than 6 and not 0\n");
-}
-return (0);
-}
+	int n;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 110);
+	else
+	{
+	printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	}
+	return (0);
+	}
