c05ce97ea363ad0ad8e197cd6c12858da215012e
Obiorah-Dennis <obiorahdennis@yahoo.com>
2023-01-02 11:53:28 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,11 +7,12 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack  != '\0''; haystack++)
+	for (; *haystack != '\0'; haystack++)
 	{
 		char *l = haystack;
 		char *p = needle;
-		while (*l == *p && *p!= '\0')
+
+		while (*l == *p && *p != '\0')
 		{
 			l++;
 			p++;
@@ -21,3 +22,4 @@ char *_strstr(char *haystack, char *needle)
 	}
 	return (0);
 }
+
