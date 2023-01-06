062b7ffdacb69a0f90a34767e43f52c1c1a1d4e1
Obiorah-Dennis <obiorahdennis@yahoo.com>
2023-01-06 04:11:35 -0800

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,17 @@
+#include<stdio.h>
 #include "main.h"
 void _puts(char *str)
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+	/**
+	 * main - Entry point
+	 *
+	 * Return: Always 0 (Success)
+	 */
+	/**
+	 *  _puts - what puts does
+	 */
+	int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
