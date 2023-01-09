edeca16451d6f503398336c1edd5bee41e021cd0
Obiorah-Dennis <obiorahdennis@yahoo.com>
2023-01-09 09:33:10 -0800

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,19 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * *create_array - creates an array of chars, and initializes
- * it with a specific char.
- * @c: char to initialize
- * @size: number of bytes to allocate
- * Return: a pointer to the array or null if it fails
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
  */
-char *create_array(unsigned int size, char c)
+char *_strdup(char *str)
 {
-	char *array = malloc(size);
+	char *duplicate;
+	unsigned int i, len;
 
-	if (size == 0 || array == 0)
+	i = 0;
+	len = 0;
+
+	if (str == NULL)
 		return (NULL);
-	while (size--)
-		array[size] = c;
-	return (array);
+
+	while (str[len])
+		len++;
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	while ((duplicate[i] = str[i]) != '\0')
+		i++;
+	return (duplicate);
 }
